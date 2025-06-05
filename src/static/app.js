82aa0e0a6bd12e8c990765e64f92b42674b5b83b
Thiago Dutra <thiagodutra@gmail.com>
2025-06-05 17:57:31 +0000

    Melhora a exibição da lista de participantes nas atividades

--- src/static/app.js
+++ src/static/app.js
@@ -25,11 +25,12 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Participants:</strong> ${
-              details.participants.length > 0
-                ? details.participants.join(", ")
-                : "No participants yet"
-            }</p>
+          <strong>Participants:</strong>
+          ${
+            details.participants.length > 0
+              ? details.participants.join(", ")
+              : "<em>No participants yet.</em>"
+          }
         `;
 
         activitiesList.appendChild(activityCard);
